c02d3677d47280059206e6b53f636bcce36ece7f
Shubham Narayan <shubhamnarayan11144@gmail.com>
2026-06-01 11:40:14 +0530

    NavBar Complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar"
 function home() {
   return (
     <div className='min-h-screen bg-[#f3f3f3] flex flex-col'>
-      hello
+      
       <Navbar/>
       
     </div>
